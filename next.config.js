0be08df989d492696541e0ef0fa7969234b6a3fa
sebastianlgonzalez <sebastianlgonzalez01@gmail.com>
2023-12-21 05:29:49 -0500

    third commit

--- next.config.js
+++ next.config.js
@@ -2,19 +2,6 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: false,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.html$/i,
-      use: 'html-loader',
-    });
-
-    return config;
-  },
-  /*
-  experimental: {
-    scrollRestoration: true,
-  }
-  */
-};
+}
 
 module.exports = nextConfig;
