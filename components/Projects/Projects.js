97fe94b76407899546a82bfed78cd21688276563
sebastianlgonzalez <sebastianlgonzalez01@gmail.com>
2023-12-21 22:48:52 -0500

    router test in main

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -1,17 +1,44 @@
 "use client"
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useSearchParams } from 'next/navigation'
 import styles from './projects.module.css'
 import Sidebar from '@/components/Sidebar/Sidebar'
 
 export default function Projects() {
   const query = useSearchParams().get('project')
+  const router = useRouter()
   return (
     <>
       <div id={styles.wrapper}>
         <h2>Projects</h2>
         <ul>
           <li>
+
+            <button 
+              data-toggled={query == "voice_assistant" ? true : false}
+              className={styles.link}
+              onClick={() => {router.replace(query == "voice_assistant" ? "/" : "/?project=voice_assistant", { scroll: false })}}
+            >
+              A.I. Voice Assistant
+            </button>
+          </li>
+          <li>
+            <button 
+              data-toggled={query == "ecoli_machine_learning" ? true : false}
+              className={styles.link}
+              onClick={() => {router.replace(query == "ecoli_machine_learning" ? "/" : "/?project=ecoli_machine_learning", { scroll: false })}}
+            >
+              Ecoli Sample Analysis with Machine Learning
+            </button>
+          </li>
+        </ul>
+      </div>
+      <Sidebar query={query} />
+    </>
+  )
+}
+/*
             <Link
               data-toggled={query == "voice_assistant" ? true : false}
               className={styles.link}
@@ -20,9 +47,8 @@ export default function Projects() {
             >
               A.I. Voice Assistant
             </Link>
-          </li>
-          <li>
-            <Link
+
+                        <Link
               data-toggled={query == "ecoli_machine_learning" ? true : false}
               className={styles.link}
               href={query == "ecoli_machine_learning" ? "/" : "/?project=ecoli_machine_learning"}
@@ -30,10 +56,5 @@ export default function Projects() {
             >
               Ecoli Sample Analysis with Machine Learning
             </Link>
-          </li>
-        </ul>
-      </div>
-      <Sidebar query={query} />
-    </>
-  )
-}
+
+*/
